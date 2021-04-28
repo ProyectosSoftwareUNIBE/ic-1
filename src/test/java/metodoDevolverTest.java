fae9488eb9d020afeead71bf40500540b88d72b2
Cristhian <Cristhian_Barros12394@hotmail.com>
2021-04-28 18:30:07 -0500

    Añadir MetodoDevolver

---
+++ src/test/java/metodoDevolverTest.java
@@ -0,0 +1,15 @@
+import junit.framework.TestCase;
+import org.junit.Before;
+
+import javax.lang.model.element.Name;
+
+public class metodoDevolverTest{
+    private user User;
+
+    @Before
+    public void before(){
+        this.User = new user("Hola","Cris");
+
+    }
+
+}
